815f03c0dbbda9d8c12c22a8471804ee728d6f31
Cysren-BK <khangtbk@gmail.com>
2021-09-08 12:51:10 +0800

    latest changes

--- server/index.js
+++ server/index.js
@@ -7,3 +7,11 @@ const app = express();
 app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: "true" }));
 app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://teoboonkhang99:Khang_990916@database.tdiv7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000 ;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log('Server running on port: ${PORT}')))
+    .catch((error) => console.log(error.message));
+
